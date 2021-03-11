7e0fb5fb4e80e4ca095347a704c8d22906216817
Ali Saeed <8asaeed@gmail.com>
2021-03-11 15:55:50 -0600

    Added loguru

--- src/executor_tests.cpp
+++ src/executor_tests.cpp
@@ -1,7 +1,11 @@
+#include "Executor.h"
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "RUNNING EXECUTOR TESTS...\n";
+    loguru::init(argc, argv);
+    loguru::add_file("clash.log", loguru::Truncate, loguru::Verbosity_MAX);
+
+    LOG_F(INFO, "RUNNING EXECUTOR TESTS...");
     return 0;
 }
